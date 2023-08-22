0e7d90a9a6fca2f2dafbf88156d0c1682554e5bc
Michael Pradetto <pradetto5@gmail.com>
2023-08-22 13:30:23 -0500

    Spring Boot Test

---
+++ src/main/kotlin/com/example/demokotlinfrancesco/DemokotlinfrancescoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demokotlinfrancesco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemokotlinfrancescoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemokotlinfrancescoApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/demokotlinfrancesco/users/UserRepository.kt
@@ -0,0 +1,3 @@
+package com.example.demokotlinfrancesco.users
+
+import
